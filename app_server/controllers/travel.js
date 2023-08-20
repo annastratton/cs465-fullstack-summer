f7cf50809f74cdcfbf30026c7390522c31f30cae
Rowan <104235853+annastratton@users.noreply.github.com>
2023-08-20 13:31:20 -0500

    code cleanup

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,48 +1,42 @@
-const request = require('request'); 
-const apiOptions = {    
-    server: 'http://localhost:3000' 
-}; 
+const request = require('request');
 
-//var fs = require('fs'); 
-//var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+const apiOptions = {
+    server: 'http://localhost:3000'
+};
 
-/* internal method to render the travel list */ 
-const renderTravelList = (req, res, responseBody) => {  
+const renderPage = (req, res, responseBody, title) => {
     let message = null;
-    let pageTitle = process.env.npm_package_description + ' - Travel';
-    if(!(responseBody instanceof Array)) {
+    if (!(responseBody instanceof Array)) {
         message = 'API lookup error';
         responseBody = [];
-    }  else {
-        if (!responseBody.length){
-            message = 'No trips exsist in our database!';
-        }
+    } else if (!responseBody.length) {
+        message = 'No trips exist in our database!';
     }
-    res.render('travel', 
-    { title: pageTitle,
+    res.render('travel', {
+        title: `${title} - Travel`,
         trips: responseBody,
         message
-    }
-    ); 
-}
+    });
+};
 
-
-/*GET travel list view */
+/* GET travel list view */
 const travelList = (req, res) => {
     const path = '/api/trips';
     const requestOptions = {
-        url: '${apiOptions.server}${path}',
+        url: `${apiOptions.server}${path}`,
         method: 'GET',
-        json: {},
+        json: {}
     };
-    console.info('>> travelController.travelList calling ' + requestOptions.url);
-    request(
-        requestOptions,
-        (err, {statusCode }, body) => {
-            if (err) {
-                console.error(err);
-            }
-            renderTravelList(req, res, body);
+    console.info(`>> travelController.travelList calling ${requestOptions.url}`);
+    request(requestOptions, (err, { statusCode }, body) => {
+        if (err) {
+            console.error(err);
         }
-    );
+        const title = process.env.npm_package_description;
+        renderPage(req, res, body, title);
+    });
+};
+
+module.exports = {
+    travelList
 };
